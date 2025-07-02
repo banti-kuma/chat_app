6465574bc78358731cabffced4b69a8209c52c64
Banti Kumar <bantikumar6142@gmail.com>
2025-07-02 21:41:22 +0530

    update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,7 +11,7 @@ import GoogleMaps
        if #available(iOS 10.0, *) {
         UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
       }
-    GMSServices.provideAPIKey("AIzaSyAqbNyKRXEeisMI0HmLS3glp1swJkU7BWc")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
